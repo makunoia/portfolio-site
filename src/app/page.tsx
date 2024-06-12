7e1c6893452d34abcfc8d2587f7eae49027a3e6f
makunoia <markbriannoya@gmail.com>
2024-06-11 22:33:43 -0400

    NextJS App setup

---
+++ src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <main>This is my main content</main>;
+}
--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,55 @@
-import express from 'express'
-import payload from 'payload'
+import dotenv from "dotenv";
+import next from "next";
+import nextBuild from "next/dist/build";
+import path from "path";
 
-require('dotenv').config()
-const app = express()
+dotenv.config({
+  path: path.resolve(__dirname, "../.env"),
+});
 
-// Redirect root to Admin panel
-app.get('/', (_, res) => {
-  res.redirect('/admin')
-})
+import express from "express";
+import payload from "payload";
 
-const start = async () => {
-  // Initialize Payload
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const start = async (): Promise<void> => {
   await payload.init({
-    secret: process.env.PAYLOAD_SECRET,
+    secret: process.env.PAYLOAD_SECRET || "",
     express: app,
-    onInit: async () => {
-      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+    onInit: () => {
+      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
     },
-  })
+  });
 
-  // Add your own express routes here
+  if (process.env.NEXT_BUILD) {
+    app.listen(PORT, async () => {
+      payload.logger.info(`Next.js is now building...`);
+      // @ts-expect-error
+      await nextBuild(path.join(__dirname, "../"));
+      process.exit();
+    });
 
-  app.listen(3000)
-}
+    return;
+  }
 
-start()
+  const nextApp = next({
+    dev: process.env.NODE_ENV !== "production",
+  });
+
+  const nextHandler = nextApp.getRequestHandler();
+
+  app.use((req, res) => nextHandler(req, res));
+
+  nextApp.prepare().then(() => {
+    payload.logger.info("Starting Next.js...");
+
+    app.listen(PORT, async () => {
+      payload.logger.info(
+        `Next.js App URL: ${process.env.PAYLOAD_PUBLIC_SERVER_URL}`
+      );
+    });
+  });
+};
+
+start();
