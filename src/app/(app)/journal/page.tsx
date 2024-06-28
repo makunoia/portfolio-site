1be0101993267e54c06eb3fed4931442c7afccf9
makunoia <markbriannoya@gmail.com>
2024-06-28 19:25:42 -0400

    WIP: Journal Item

--- src/app/(app)/journal/page.tsx
+++ src/app/(app)/journal/page.tsx
@@ -1,6 +1,8 @@
 import Text from "@/components/Text";
 import React from "react";
 import JournalSection from "@/components/PageListSection";
+import SectionDivider from "@/components/SectionDivider";
+SectionDivider;
 
 const Page = () => {
   const allEntries = [
@@ -40,9 +42,53 @@ const Page = () => {
           </Text>
         </div>
 
-        <JournalSection header="2024" data={allEntries} />
+        <div className="flex flex-col gap-16px">
+          <SectionDivider header="2024" />
+          <div className="flex flex-col gap-16px">
+            {allEntries.map(({ label, desc, tag }) => (
+              <JournalListItem
+                key={label}
+                label={label}
+                desc={desc}
+                tag={tag}
+              />
+            ))}
+          </div>
+        </div>
       </main>
     </>
+  );
+};
+
+type JournalListItemProps = {
+  label: string;
+  desc: string;
+  tag: string;
+};
+
+const JournalListItem = ({ label, desc, tag }: JournalListItemProps) => {
+  return (
+    <div className="flex flex-row justify-between cursor-pointer">
+      <div>
+        <div className="flex flex-col gap-4px">
+          <Text size="body">{label}</Text>
+          <Text size="caption" className="text-subtle">
+            {desc}
+          </Text>
+        </div>
+        <div className="flex items-center rounded-10px bg-subtle/40 px-10px py-2px">
+          <Text size="caption" className="text-subtle text-nowrap">
+            {tag}
+          </Text>
+        </div>
+      </div>
+      <div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas
+        recusandae repellendus harum doloribus placeat necessitatibus veritatis
+        cumque odit, illum, quis deleniti id dicta assumenda qui tenetur optio
+        cum voluptatem similique!
+      </div>
+    </div>
   );
 };
 
