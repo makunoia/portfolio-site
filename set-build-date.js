66c752a847319d0296ab02563ff932d17216f73b
makunoia <markbriannoya@gmail.com>
2024-07-26 00:28:28 -0400

    fix/set-build-date

--- set-build-date.js
+++ set-build-date.js
@@ -1,5 +1,4 @@
 import env from "@next/env";
-
 env.loadEnvConfig("./");
 
 function formatBuildDate(date) {
@@ -12,40 +11,35 @@ function formatBuildDate(date) {
     hour12: true,
   }).format(date);
 }
-async function updateBuildDate() {
-  const buildDate = new Date();
-  const formattedBuildDate = formatBuildDate(buildDate);
 
-  const token = process.env.VERCEL_TOKEN;
-  const id = process.env.VERCEL_BUILD_DATE_ENV_ID;
-  const projectID = process.env.VERCEL_PROJECT_ID;
+let data;
+const buildDate = new Date();
+const formattedBuildDate = formatBuildDate(buildDate);
 
-  if (!token || !id || !projectID) {
-    console.error("Environment variables are missing.");
-    return;
-  }
+const token = process.env.VERCEL_TOKEN;
+const id = process.env.VERCEL_BUILD_DATE_ENV_ID;
+const projectID = process.env.VERCEL_PROJECT_ID;
+const fetchURL = `https://api.vercel.com/v9/projects/${projectID}/env/${id}`;
 
-  try {
-    const response = await fetch(
-      `https://api.vercel.com/v9/projects/${projectID}/env/${id}`,
-      {
-        body: { value: formatBuildDate },
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-        method: "PATCH",
-      }
-    );
-
-    if (!response.ok) {
-      throw new Error(`Failed to update: ${response.statusText}`);
-    }
-
-    const data = await response.json();
-    console.log("Environment variable updated:", data);
-  } catch (error) {
-    console.error("Error updating environment variable:", error);
-  }
+if (!token || !id || !projectID) {
+  console.error("Environment variables are missing.");
 }
+try {
+  const response = await fetch(fetchURL, {
+    method: "PATCH",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ value: formattedBuildDate }), // Stringify the body
+  });
 
-updateBuildDate();
+  if (!response.ok) {
+    throw new Error(`Failed to update: ${response.statusText}`);
+  }
+
+  data = await response.json();
+  console.log("Environment variable updated:", data);
+} catch (error) {
+  console.error("Error updating environment variable:", error);
+}
