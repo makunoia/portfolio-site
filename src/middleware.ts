bee7386da2733fd8da254933e86ef8580f4a1bd1
makunoia <markbriannoya@gmail.com>
2024-08-22 18:37:03 -0400

    fix/middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,17 @@
 import { v4 as uuidv4 } from "uuid";
 import { NextRequest, NextResponse } from "next/server";
 
-const response = NextResponse.next();
-
-const assignUUID = async (request: NextRequest) => {
+export function middleware(request: NextRequest) {
   const cookies = request.cookies;
   let userUUID = cookies.get("userUUID")?.value;
+  const response = NextResponse.next();
+
   if (!userUUID) {
     userUUID = uuidv4();
-    return response.cookies.set({ name: "userUUID", value: userUUID });
+    response.cookies.set({ name: "userUUID", value: userUUID });
   }
-};
 
-export async function middleware(request: NextRequest) {
-  //implement a function that checks for an auth header
-  // if header is not found or if it's false, redirect to a locked screen page
-  // if header is found and user is auth, redirect to the "locked" project page
-
-  return assignUUID(request);
+  return response;
 }
 
 export const config = {
