c755f0bd9fa5912b440066c515638e20302535d0
makunoia <markbriannoya@gmail.com>
2024-08-22 00:13:24 -0400

    Removed headers in next config

--- next.config.mjs
+++ next.config.mjs
@@ -36,55 +36,6 @@ const nextConfig = {
     loader: "custom",
     loaderFile: "./src/app/(app)/lib/loader.ts",
   },
-  async headers() {
-    return [
-      {
-        source: "/projects/:project",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "max-age=0, s-maxage=1, stale-while-revalidate=59",
-          },
-        ],
-      },
-      {
-        source: "/projects/[project]",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "max-age=0, s-maxage=1, stale-while-revalidate=59",
-          },
-        ],
-      },
-      {
-        source: "/",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "max-age=0, s-maxage=86400, stale-while-revalidate=59",
-          },
-        ],
-      },
-      {
-        source: "/projects",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "max-age=0, s-maxage=86400, stale-while-revalidate=59",
-          },
-        ],
-      },
-      {
-        source: "/journal",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "max-age=0, s-maxage=86400, stale-while-revalidate=59",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default withPayload(nextConfig);
