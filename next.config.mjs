f0c0b809b7d2c52293a1ea961bdb3546507cdb10
makunoia <markbriannoya@gmail.com>
2024-08-22 22:42:00 -0400

    feat/bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 import { withPayload } from "@payloadcms/next/withPayload";
+import bundleAnalyzer from "@next/bundle-analyzer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     ppr: "incremental",
     reactCompiler: false,
+    optimizePackageImports: ["icon-library"],
   },
   images: {
     remotePatterns: [
@@ -37,5 +39,14 @@ const nextConfig = {
     loaderFile: "./src/app/(app)/lib/loader.ts",
   },
 };
+
+// Bundle analyzer
+// const withBundleAnalyzer = bundleAnalyzer({
+//   enabled: process.env.ANALYZE === "true",
+// });
+
+// Combine withPayload and withBundleAnalyzer
+// const combinedConfig = withPayload(withBundleAnalyzer(nextConfig));
+// export default combinedConfig;
 
 export default withPayload(nextConfig);
