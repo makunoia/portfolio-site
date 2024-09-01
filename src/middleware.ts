a007c057997bb8c28f09558480c049049dbc4af1
makunoia <markbriannoya@gmail.com>
2024-09-01 13:30:01 -0400

    Removed BASE_URL env variable

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,15 +2,12 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import Users from "./app/(payload)/collections/Users";
 
-export const getLockedPages = async () => {
-  const res = await fetch(
-    `${process.env.BASE_URL}/api/projects?where[isLocked][equals]=true`,
-    {
-      headers: {
-        Authorization: `${Users.slug} API-Key ${process.env.PAYLOAD_API_KEY}`,
-      },
-    }
-  );
+export const getLockedPages = async (host: string) => {
+  const res = await fetch(`${host}/api/projects?where[isLocked][equals]=true`, {
+    headers: {
+      Authorization: `${Users.slug} API-Key ${process.env.PAYLOAD_API_KEY}`,
+    },
+  });
   const response = await res.json();
 
   const lockedPages = response.docs
@@ -20,13 +17,15 @@ export const getLockedPages = async () => {
 };
 
 export async function middleware(request: NextRequest) {
-  console.log(request.nextUrl.pathname);
-
   const { pathname } = request.nextUrl;
   const authCookie = request.cookies.get("auth");
   const isPageView = request.headers.get("purpose") !== "prefetch";
 
-  const lockedProjects: string[] = await getLockedPages();
+  const protocol = request.headers.get("x-forwarded-proto") || "http";
+  const host = request.headers.get("host") || "localhost";
+  const baseUrl = `${protocol}://${host}`;
+
+  const lockedProjects: string[] = await getLockedPages(baseUrl);
   const isLocked = lockedProjects.includes(pathname.split("/").pop() as string);
 
   if (isPageView) {
