e980110d0b87dfe7e8b091a0be4c0029bfda30c3
Mark Noya <markbriannoya@gmail.com>
2025-09-30 11:57:41 +0800

    Improved caching for Resume and Password

--- src/app/(app)/lib/globals.ts
+++ src/app/(app)/lib/globals.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import config from "@payload-config";
+import {unstable_cache} from "next/cache";
 import {getPayload} from "payload";
 import type {AccessPassword, Resume} from "payload-types";
 
@@ -14,15 +15,31 @@ const getPayloadClient = async () => {
   return payloadClient;
 };
 
+const getAccessPasswordsGlobalCached = unstable_cache(
+  async () => {
+    const payload = await getPayloadClient();
+    return payload.findGlobal({slug: "access-passwords"});
+  },
+  ["global:access-passwords"],
+  {tags: ["global", "global:access-passwords"]}
+);
+
 export const getAccessPasswordsGlobal = async (): Promise<AccessPassword> => {
-  const payload = await getPayloadClient();
-  return payload.findGlobal({slug: "access-passwords"});
+  return getAccessPasswordsGlobalCached();
 };
 
+const getResumeGlobalCached = unstable_cache(
+  async () => {
+    const payload = await getPayloadClient();
+    return payload.findGlobal({
+      slug: "resume",
+      depth: 1,
+    });
+  },
+  ["global:resume"],
+  {tags: ["global", "global:resume"]}
+);
+
 export const getResumeGlobal = async (): Promise<Resume> => {
-  const payload = await getPayloadClient();
-  return payload.findGlobal({
-    slug: "resume",
-    depth: 1,
-  });
+  return getResumeGlobalCached();
 };
